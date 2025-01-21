5f1e41af705d1eb543693f569d4c2b2fb0780354
Abdukholiq Tojiboev <tojiboevabdukholiq@gmail.com>
2025-01-21 18:28:45 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   print('Фоновое сообщение: ${message.messageId}');
-// }
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
   runApp(MyApp());
 }
 
